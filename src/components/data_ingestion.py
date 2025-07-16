9f50041fe7ec1e35479f06f84f5870d1f9a56943
Rohit Labade <rohitlabade777@gmail.com>
2025-07-16 23:38:33 +0530

    DATA Transformation and Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts",'train.csv')
@@ -42,4 +47,9 @@ class DataIngesion:
 
 if __name__=="__main__":
     obj=DataIngesion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    Modeltrainer=ModelTrainer()
+    print(Modeltrainer.initiate_model_trainer(train_arr,test_arr))
